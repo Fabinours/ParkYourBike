b88c2edb113e687cf61e6451cd66f5f996e7b037
Fabinours <44270057+Fabinours@users.noreply.github.com>
2020-09-25 18:57:04 +0200

    last_update

--- PYB/PYB.py
+++ PYB/PYB.py
@@ -1,37 +1,33 @@
+#IMPORTS
 import sys
 import plotly
 import plotly.express as px
 import pandas as pd
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-
-def main():
-    pass
-
+#PROGRAMME
 if __name__ == "__main__":
     
+    #Lecture des données
     df = pd.read_csv('parkings.csv',sep=';')
 
-    fig1 = px.histogram(df, x="capacite",range_x=[0,100])
-    #fig.show()
+    #Création de l'histogramme
+    fig1 = px.histogram(df, x="capacite",title="Histogramme du nombre de stationnements pour vélos en fonction de leur capacité d\'accueil")
 
-    fig2 = px.scatter_mapbox(df, lat="Lat", lon="Lon", hover_name="osm_id", hover_data=["type", "capacite","acces","payant","surveille","couvert"],color="capacite", zoom=9, title="Carte des stationnements pour vélos en Ile de France",size="capacite", color_continuous_scale=px.colors.diverging.RdYlGn,range_color=[0,50])
+    #Création de la carte intéractive
+    fig2 = px.scatter_mapbox(df, lat="Lat", lon="Lon", hover_name="osm_id", hover_data=["type", "capacite","acces","payant","surveille","couvert"],color="capacite", zoom=9, title="Carte des stationnements pour vélos en Île de France",size="capacite", color_continuous_scale=px.colors.diverging.RdYlGn,range_color=[0,50])
+    #Type d'affichage de la carte
     fig2.update_layout(mapbox_style="light", mapbox_accesstoken="pk.eyJ1IjoiaGVyZWFsIiwiYSI6ImNrMW92ZnJ3dDBvaWQzbWw4MWMyemRmMTkifQ.ybaNjSTBRj1Cw45T379ZMA")
-    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    #fig.show()
 
+    #Disposition des éléments graphiques de la page 
     app = dash.Dash()
     app.layout = html.Div([
-        html.H1(children='Histogramme du nombre de stationnements pour vélos par leur capacité d\'accueil'),
-        dcc.Graph(figure=fig1),
-        html.H1(children='Carte des stationnements pour vélos en Ile de France'),
-        dcc.Graph(figure=fig2)
+        dcc.Graph(figure=fig1),#histogramme
+        dcc.Graph(figure=fig2)#carte
     ])
-
+    #Lancement de la page
     app.run_server(debug=True, use_reloader=False)
 
     
