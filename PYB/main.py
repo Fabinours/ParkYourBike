ebf67aef53c1e7c6c7991dc8d7780f801c4a326b
Loïc FOURNIER <46942069+Hereal@users.noreply.github.com>
2020-09-28 11:49:42 +0200

    update

--- PYB/main.py
+++ PYB/main.py
@@ -6,7 +6,38 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import requests
+import csv
 
+def downloadCsv():
+    url = 'https://data.iledefrance.fr/explore/dataset/stationnement-velo-en-ile-de-france/download?format=csv'
+    r = requests.get(url, allow_redirects=True)
+    open('test.csv', 'wb').write(r.content)
+
+
+def modifyCsv():
+    lst=[]
+    index = 1
+    reader = csv.reader(open('test.csv', 'r'),delimiter=";")
+    for row in reader:
+        for i in range(0,len(row)):
+            if(row[i]=="geo_point_2d"):
+                index = i;
+        lst.append(row)
+    lst[0].pop(index)
+    lst[0].append("Lat")
+    lst[0].append("Lon")
+    for i in range(1,len(lst)):
+        value = lst[i].pop(index)
+        value = value.split(",")
+        lst[i].append(value[0])
+        lst[i].append(value[1])
+
+    f = open('numbers3.csv', 'w')
+    with f:
+        writer = csv.writer(f,delimiter=';')
+        writer.writerows(lst)
+       
 
 #Ajouter x figures au dashboard
 def joinFigures(app,*figures):
@@ -18,9 +49,12 @@ def joinFigures(app,*figures):
 
 #PROGRAMME
 if __name__ == "__main__":
+
+    downloadCsv()
+    modifyCsv()
     
     #Lecture des données
-    df = pd.read_csv('parkings.csv',sep=';')
+    df = pd.read_csv('numbers3.csv',sep=';')
 
     #Création de l'histogramme
     fig1 = px.histogram(df, x="capacite",title="Histogramme du nombre de stationnements pour vélos en fonction de leur capacité d\'accueil")
@@ -36,5 +70,5 @@ if __name__ == "__main__":
     joinFigures(app,fig1,fig2)
     #Lancement de la page
     app.run_server(debug=True, threaded=True,use_reloader=False)
-
+    
     
